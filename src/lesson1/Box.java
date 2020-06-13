56b0f5c44e6ec3969480d8a1c41c6cecfcf9a4ac
senyabird <senyabird@yandex.ru>
2020-06-13 14:17:23 +0300

    Added getWeight Method in Box

--- src/lesson1/Box.java
+++ src/lesson1/Box.java
@@ -1,4 +1,20 @@
 package lesson1;
 
+import java.util.ArrayList;
+
 public class Box<T extends Fruit> {
+
+    private ArrayList<T> fruits = new ArrayList<>();
+
+    public Box(ArrayList<T> fruits) {
+        this.fruits = fruits;
+    }
+
+    public float getWeight() {
+        float weight = 0;
+        for (T fruit: fruits) {
+            weight += fruit.getWeight();
+        }
+        return weight;
+    }
 }
