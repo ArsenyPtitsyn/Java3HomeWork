b912fa27579438df28b19a4df1c904c8a15a678e
senyabird <senyabird@yandex.ru>
2020-06-16 18:46:32 +0300

    copy, paste

--- network/src/ru/gb/jthree/network/SocketThread.java
+++ network/src/ru/gb/jthree/network/SocketThread.java
@@ -1,4 +1,66 @@
 package ru.gb.jthree.network;
 
-public class SocketThread {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SocketThread extends Thread{
+
+    private final SocketThreadListener listener;
+    private final Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public SocketThread(String name, SocketThreadListener listener, Socket socket) {
+        super(name);
+        this.listener = listener;
+        this.socket = socket;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            listener.onSocketStart(this, socket);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            listener.onSocketReady(this, socket);
+            while (!isInterrupted()) {
+                String msg = in.readUTF();
+                listener.onReceiveString(this, socket, msg);
+            }
+        } catch (IOException e) {
+            listener.onSocketException(this, e);
+        } finally {
+            close();
+        }
+    }
+
+    public boolean sendMessage(String msg) {
+        try {
+            out.writeUTF(msg);
+            out.flush();
+            return true;
+        } catch (IOException e) {
+            listener.onSocketException(this, e);
+            close();
+            return false;
+        }
+    }
+
+    public void close() {
+        try {
+            in.close();
+        } catch (IOException e) {
+            listener.onSocketException(this,e);
+        }
+        interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            listener.onSocketException(this, e);
+        }
+        listener.onSocketStop(this);
+    }
 }
