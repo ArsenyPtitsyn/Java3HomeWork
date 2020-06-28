fea7bdffad888bcca50f7cd48ad0c84dfd6334fd
senyabird <senyabird@yandex.ru>
2020-06-28 23:58:26 +0300

    CountDownLatch attached

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -1,6 +1,6 @@
 package lesson5;
 
-public class Car implements Runnable {
+public class Car {
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
@@ -8,12 +8,8 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
-    public String getName() {
-        return name;
-    }
-    public int getSpeed() {
-        return speed;
-    }
+
+
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
@@ -21,17 +17,11 @@ public class Car implements Runnable {
         this.name = "Участник #" + CARS_COUNT;
     }
 
-    @Override
-    public void run() {
-        try {
-            System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
-            System.out.println(this.name + " готов");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
-        }
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 }
--- src/lesson5/MainClass.java
+++ src/lesson5/MainClass.java
@@ -1,16 +1,33 @@
 package lesson5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
+        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            final Car c = cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            new Thread(() -> {
+                try {
+                    System.out.println(c.getName() + " готовится");
+                    Thread.sleep(500 + (int)(Math.random() * 800));
+                    System.out.println(c.getName() + " готов");
+                    cdl.countDown();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
